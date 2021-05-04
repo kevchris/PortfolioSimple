65df2f0ef03859194863be24e1e9ddd0cafe6217
Kevin Christopher <kevinchristopher7777@gmail.com>
2021-05-04 09:58:17 +0530

    Jobs working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your Blog model here.
+class Blog(models.Model):
+    title=models.CharField(max_length=255)
+    pubDate=models.DateTimeField(auto_now_add=True)
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+# title, published date, body, image
+# Add blog app to settings
+#create a migration
+#migrate
+#add to the admin
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import job
 # Create your views here.
+ home(request):
+    jobs=Job.objects
+    return render(request,'jobs/home.html',{'jobs': jobs})
